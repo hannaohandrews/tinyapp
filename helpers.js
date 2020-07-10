11c41e718c2b834a28f339cbb296e28b9a77b201
hannaohandrews <hannaohandrews@gmail.com>
2020-07-10 23:41:33 +0000

    done

---
+++ helpers.js
@@ -0,0 +1,12 @@
+
+const getUserByEmail = function(email,users) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return user;
+    }
+  }
+};
+
+
+
+module.exports = { getUserByEmail };
